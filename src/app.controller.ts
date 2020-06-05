4b394d570fd58bc0942d76009eb07860a67807da
xiaodong.li <xiaodong.li@asiainnovations.com>
2020-06-05 16:47:23 +0800

    add @User() decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,8 @@ import { Controller, UseGuards, Post, Request, Get } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/guard/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
+import { User } from './common/decorators/user.decorator';
+import { JwtPayload } from './auth/constants';
 
 @Controller()
 export class AppController {
@@ -18,4 +20,11 @@ export class AppController {
   getProfile(@Request() req) {
     return req.user;
   }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('user')
+  getUser(@User() user: JwtPayload) {
+    return user;
+  }
+
 }
