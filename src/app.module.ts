ac32504ce4ea830939ce81e123738aeb7e78d70c
xiaodong.li <xiaodong.li@asiainnovations.com>
2020-06-05 15:11:22 +0800

    local-password auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
